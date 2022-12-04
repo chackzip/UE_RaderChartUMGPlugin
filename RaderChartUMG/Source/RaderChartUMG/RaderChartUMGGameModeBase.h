2e0f2b1cc1d45d778c693059d3af3c6dfd76fb9e
chackzip <chackzip@yahoo.co.jp>
2022-12-04 13:26:44 +0900

    プロジェクト作成

---
+++ RaderChartUMG/Source/RaderChartUMG/RaderChartUMGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RaderChartUMGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RADERCHARTUMG_API ARaderChartUMGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
